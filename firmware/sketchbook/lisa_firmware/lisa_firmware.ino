5242075546b219a2b7aff22f10cf0bf3a5c555e9
Anton Mitrokhin <anton.mitrokhin@phystech.edu>
2019-03-13 14:06:08 -0400

    Move motor control from servo to pwm

--- firmware/sketchbook/lisa_firmware/lisa_firmware.ino
+++ firmware/sketchbook/lisa_firmware/lisa_firmware.ino
@@ -5,35 +5,57 @@
 #include <geometry_msgs/Twist.h>
 
 ros::NodeHandle  nh;
+static int motor_pwm_pin = 21;
+static int motor_dir_pin = 22;
 
 Servo servo_steering;
-Servo servo_motor;
 
 static int steering_midpoint = 90;
-static int motor_midpoint = 90;
+static unsigned int watchdog = 0;
 
 void velocity_cb(const geometry_msgs::Twist& cmd_msg){
-  servo_motor.write(motor_midpoint + cmd_msg.linear.x * 300);
-  servo_steering.write(steering_midpoint + cmd_msg.angular.z * 40);  
+  watchdog = 0;
+  servo_steering.write(steering_midpoint + cmd_msg.angular.z * 40);
+  if (cmd_msg.linear.x > 0) {
+    digitalWrite(motor_dir_pin, HIGH);
+  } else {
+    digitalWrite(motor_dir_pin, LOW);
+  }
+  analogWrite(motor_pwm_pin, int(abs(cmd_msg.linear.x)));
+}
+
+void reset_car() {
+  digitalWrite(motor_pwm_pin, LOW);  
+  digitalWrite(motor_dir_pin, HIGH);
+  analogWrite(motor_pwm_pin, 0);
+  servo_steering.write(steering_midpoint);
 }
 
 
-ros::Subscriber<geometry_msgs::Twist> velocity_sub("/cmd_vel_mux/input/teleop", velocity_cb);
+ros::Subscriber<geometry_msgs::Twist> velocity_sub("/cmd_vel_mux/motor_raw", velocity_cb);
 
 void setup(){
-  pinMode(13, OUTPUT);
+  pinMode(13, OUTPUT); // led
+  pinMode(motor_pwm_pin, OUTPUT); // motor pwm
+  pinMode(motor_dir_pin, OUTPUT); // motor direction
+  digitalWrite(motor_pwm_pin, LOW);  
+  digitalWrite(motor_dir_pin, HIGH);
 
   nh.initNode();
   nh.subscribe(velocity_sub);
   
   servo_steering.attach(20);
-  servo_motor.attach(21);
-
   servo_steering.write(steering_midpoint);
-  servo_motor.write(motor_midpoint);
+
+  watchdog = 0;
 }
 
 void loop(){
   nh.spinOnce();
   delay(1);
+  watchdog ++;
+
+  if (watchdog >= 1000) {
+    reset_car();
+  }
 }
